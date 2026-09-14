5cfe9a229a67b729568e773c6087b1feb821c37c
dseynabou774-debug <dseynabou774@gmail.com>
2026-09-14 07:41:06 +0000

    Add files via upload

---
+++ sw-5.js
@@ -0,0 +1,24 @@
+// Incrémente ce numéro à chaque nouvelle mise à jour du fichier index.html
+const CACHE_VERSION = 'epargne-tablette-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_VERSION).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
